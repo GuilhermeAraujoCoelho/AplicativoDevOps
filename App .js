74cfc70744f616e6b99d27f557d5d4388669c010
GuilhermeAraujoCoelho <161751341+GuilhermeAraujoCoelho@users.noreply.github.com>
2024-05-17 21:52:59 -0300

    Create App.js
    Criação do arquivo app j.s que verifica se o número é par ou impar

---
+++ App .js
@@ -0,0 +1,9 @@
+// Definindo uma lista de números
+let de numeros = [2,4,6,8,10]
+//Percorrendo a lista com um loop for
+for(let i = 0; i < numeros .length; 1 ++){
+  if (numero[i] % 2 === 0){
+console.log(numero[i] + " é um número par);
+      }else{
+console.log(numero{1} + " é um numerp Ímpar);
+  }
